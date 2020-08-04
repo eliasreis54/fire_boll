99237462718602f9368bc550363bc8c2c4baa5c9
Elias Reis <eliasreis54@gmail.com>
2020-08-03 21:03:38 -0300

    refactor to use flame text render

--- lib/points.dart
+++ lib/points.dart
@@ -1,19 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flame/text_config.dart';
+import 'package:flame/position.dart';
 
 class PointsRender {
+  final TextConfig config = TextConfig(
+      fontSize: 25.0,
+      color: Colors.white,
+  );
+  
   void render(Canvas canvas, int points, Size screenSize) {
-    final width = ((screenSize.width / 2) - 30);
-    final position = Offset(width, 100);
-
-    final text = TextPainter(
-      text: TextSpan(
-        text: 'Pontos: $points',
-        style: TextStyle(
-          color: Colors.white,
-        ),
-      ),
-      textDirection: TextDirection.ltr,
-    )..layout(maxWidth: width);
-    text.paint(canvas, position);
+    double x = (screenSize.width / 2) - 50;
+    double y = (screenSize.height / 6);
+    config.render(canvas, "Pontos: $points", Position(x, y));
   }
 }
